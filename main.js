6865df90ebe6d0c42add611cbcbaf55a109e5963
israksadi28 <israksadi28@gmail.com>
2022-03-17 16:27:04 +0600

    My portfolio website

---
+++ main.js
@@ -0,0 +1,43 @@
+// Sticky Navbar
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+// Dark Mode
+let darkmode = document.querySelector("#darkmode");
+const currentTheme = localStorage.getItem("currentTheme");
+
+if (currentTheme) {
+  document.body.classList.add("active");
+}
+
+darkmode.addEventListener("click", () => {
+  document.body.classList.toggle("active");
+
+  if (darkmode.classList.contains("bx-moon")) {
+    darkmode.classList.replace("bx-moon", "bx-sun");
+
+    // document.body.classList.add("active");
+  } else {
+    darkmode.classList.replace("bx-sun", "bx-moon");
+
+    // document.body.classList.remove("active");
+  }
+
+  if (document.body.classList.contains("active")) {
+    localStorage.setItem("currentTheme", "themeActive");
+  } else {
+    localStorage.removeItem("currentTheme");
+  }
+});
